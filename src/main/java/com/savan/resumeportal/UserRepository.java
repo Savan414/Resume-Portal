700b3af347da49757fd2e0fc1bc6ebb34cf90672
Savan Bhanderi <bsavan414@gmail.com>
2021-11-05 16:52:21 -0400

    Adding User JPA support for Spring security

--- src/main/java/com/savan/resumeportal/UserRepository.java
+++ src/main/java/com/savan/resumeportal/UserRepository.java
@@ -1,11 +1,11 @@
 package com.savan.resumeportal;
 
-import  com.savan.resumeportal.nodels.User;
-import org.springfranework.data.jpa.repository.JpaRepository;
+import com.savan.resumeportal.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-        Optional<User> findByuserName(String userNane);
+        Optional<User> findByUserName(String userName);
 
 }
