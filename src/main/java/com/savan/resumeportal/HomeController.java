39d67bb8b9dbaf1f19608e59f5987b2b487374ac
Savan Bhanderi <bsavan414@gmail.com>
2021-11-06 17:45:39 -0400

    Added user profile entity and fetching to show theme and summary

--- src/main/java/com/savan/resumeportal/HomeController.java
+++ src/main/java/com/savan/resumeportal/HomeController.java
@@ -1,16 +1,60 @@
 package com.savan.resumeportal;
 
+import com.savan.resumeportal.models.Job;
+import com.savan.resumeportal.models.MyUserDetails;
+import com.savan.resumeportal.models.UserProfile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    UserProfileRepository userProfileRepository;
+
     @GetMapping("/")
     public String home(){
-        return "hello";
+
+
+        Optional<UserProfile> profileOptional = userProfileRepository.findByUserName("einstein");
+        profileOptional.orElseThrow(() -> new RuntimeException("Not found: "));
+
+        UserProfile profile1 = profileOptional.get();
+
+        Job job1 = new Job();
+        job1.setCompany("Company 1");
+        job1.setDesignation("Designation");
+        job1.setId(1);
+        job1.setStartDate(LocalDate.of(2020, 1, 1));
+        job1.setEndDate(LocalDate.of(2020, 3, 1));
+
+        Job job2 = new Job();
+        job2.setCompany("Company 2");
+        job2.setDesignation("Designation");
+        job2.setId(2);
+        job2.setStartDate(LocalDate.of(2019, 5, 1));
+        job2.setEndDate(LocalDate.of(2020, 1, 1));
+
+        List<Job> jobs = new ArrayList<Job>();
+        jobs.add(job1);
+        jobs.add(job2);
+        profile1.getJobs().clear();
+        profile1.getJobs().add(job1);
+        profile1.getJobs().add(job2);
+
+        userProfileRepository.save(profile1);
+
+        return "Profile";
     }
 
     @GetMapping("/edit")
@@ -20,7 +64,15 @@ public class HomeController {
 
     @GetMapping("/view/{userId}")
     public String view(@PathVariable String userId, Model model){
+        Optional<UserProfile> userProfileOptional = userProfileRepository.findByUserName(userId);
+        userProfileOptional.orElseThrow(() -> new RuntimeException("Not found: " + userId));
+
         model.addAttribute("userId", userId);
-        return "profile-templates/index";
+        UserProfile userProfile = userProfileOptional.get();
+        model.addAttribute("userProfile", userProfile);
+
+        System.out.println(userProfile.getJobs().toString());
+
+        return "profile-templates/" + userProfile.getTheme() + "/index";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
 insert into user (id, user_name, password, active, roles) values
-(1, 'foo', 'foo', true, 'USER'),
-(2, 'bar', 'bar', true, 'USER');
+(1, 'einstein', 'einstein', true, 'USER'),
+(2, 'newton', 'newton', true, 'USER');
+
+insert into user_profile (id, user_name, theme, summary, first_name, last_name, email, phone, designation) values
+(1, 'einstein', '1', 'Deeloped the theroy of relativity, one of the two pillars of modern physics, My work is also known for its influence on the philosophy of science.',
+'Albert', 'Einstein', 'einstein@gmail.com', '647-849-4193', 'Theoretical physicist'),
+(2, 'newton', '2', 'Widely recognised as one of he most influential scientists of all time and as a key figure in the philosophical revolution known as the Enlightenment',
+'Isaac', 'Newton', 'newton@gmail.com', '241-694-7416', 'Mathematician, physicist, astronomer, theologian, and author');
