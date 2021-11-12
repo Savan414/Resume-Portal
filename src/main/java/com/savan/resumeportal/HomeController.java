19e0e38e6a7f6e267a471d0008c1e3e4aba2ea0d
Savan Bhanderi <bsavan414@gmail.com>
2021-11-12 11:44:37 -0500

    Added the functionality to add new content in the resume

--- src/main/java/com/savan/resumeportal/HomeController.java
+++ src/main/java/com/savan/resumeportal/HomeController.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.time.LocalDate;
@@ -92,22 +90,34 @@ public class HomeController {
     }
 
     @GetMapping("/edit")
-    public String edit(Model model,Principal principal){
+    public String edit(Model model, Principal principal, @RequestParam(required = false) String add){
         String userId = principal.getName();
         model.addAttribute("userId", userId);
         Optional<UserProfile> userProfileOptional = userProfileRepository.findByUserName(userId);
         userProfileOptional.orElseThrow(() -> new RuntimeException("Not found: " + userId));
         UserProfile userProfile = userProfileOptional.get();
 
+        if("job".equals(add)){
+            userProfile.getJobs().add(new Job());
+        } else if("education".equals(add)){
+            userProfile.getEducations().add(new Education());
+        } else if("skill".equals(add)){
+            userProfile.getSkills().add("");
+        }
         model.addAttribute("userProfile", userProfile);
         return "profile-edit";
     }
 
     @PostMapping("/edit")
-    public String postEdit(Model model,Principal principal){
-        String userId = principal.getName();
-       // Save the updated values in the form
-        return "redirect:/view/" + userId;
+    public String postEdit(Model model, Principal principal, @ModelAttribute UserProfile userProfile){
+        String userName = principal.getName();
+        Optional<UserProfile> userProfileOptional = userProfileRepository.findByUserName(userName);
+        userProfileOptional.orElseThrow(() -> new RuntimeException("Not found: " + userName));
+        UserProfile savedUserProfile = userProfileOptional.get();
+        userProfile.setId(savedUserProfile.getId());
+        userProfile.setUserName(userName);
+        userProfileRepository.save(userProfile);
+        return "redirect:/view/" + userName;
     }
 
     @GetMapping("/view/{userId}")
